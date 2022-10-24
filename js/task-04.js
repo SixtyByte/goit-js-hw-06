720ecef5aa30a0d07bfe34e05077dff4cc13436f
SixtyByte <valentinvodopyan@ukr.net>
2022-10-24 19:25:42 +0300

    4

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const plusButton=document.querySelector(` button[data-action="increment"]`)
+console.log(plusButton)
+const minusButton=document.querySelector(`button[data-action="decrement"]`)
+console.log(minusButton)
+const clickMetr=document.querySelector("#value")
+console.log (clickMetr)
+let counterValue=0
+plusButton.addEventListener(`click`,handlePlusClick)
+minusButton.addEventListener(`click`,handleMinusClick)
+
+
+function handlePlusClick() {clickMetr.textContent=`${counterValue}`;
+counterValue+=1
+}
+function handleMinusClick(){clickMetr.textContent=`${counterValue}`;counterValue-=1}
