631699d4f6e49935b4b7a52f35f556992a3b16cc
SixtyByte <valentinvodopyan@ukr.net>
2022-10-25 20:01:34 +0300

    Ready 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formItem = document.querySelector(".login-form");
 
+formItem.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заповни поля");
+  }
+
+  console.log (`email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+
+const magicButton=document.querySelector(`.change-color`)
+
+const colorText=document.querySelector(`.color`)
+console.log(colorText.textContent)
+magicButton.addEventListener(`click`, clickmagic);
+function clickmagic(){
+const colorGen = getRandomHexColor();
+
+document.body.setAttribute('style', `background-color:${colorGen}`)
+colorText.textContent=colorGen
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
